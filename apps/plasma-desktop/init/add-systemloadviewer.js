09e9692fb6b41a3de22fc5e79e95a16953b7a1b8
Diederik de Haas <github@cknow.org>
2013-12-05 23:59:43 +0100

    Set the index of the systemloadviewer to the index of the digital-clock.
    Hopefully this makes it appear just to the left of the digital clock.

--- apps/plasma-desktop/init/add-systemloadviewer.js
+++ apps/plasma-desktop/init/add-systemloadviewer.js
@@ -1,6 +1,7 @@
 var findObjects = loadTemplate('org.cknow.plasma-desktop-scripting.findObjects');
 var pnl = findObjects.findPanelByName("defaultPanel");
 var sysldvwr = findObjects.findWidgetByType("systemloadviewer");
+var datetime = findObjects.findWidgetByType("digital-clock");
 
 if( pnl == null || typeof pnl == "undefined") {
    pnl = panels()[0];
@@ -9,5 +10,6 @@ if( pnl == null || typeof pnl == "undefined") {
 if( sysldvwr == null || typeof sysldvwr == "undefined") {
    sysldvwr = pnl.addWidget("systemloadviewer");
    sysldvwr.writeConfig("show_multiple_cpus", true);
+   sysldvwr.index = datetime.index;
 }
 
