d1a27027d859054b9f6591eae1ecfb42f0b815b5
Diederik de Haas <github@cknow.org>
2013-12-06 00:34:34 +0100

    Added script to configure the clock for dutch locale.

---
+++ apps/plasma-desktop/init/52-config-digitalclock.js
@@ -0,0 +1,12 @@
+var findObjects = loadTemplate('org.cknow.plasma-desktop-scripting.findObjects');
+
+var datetime = findObjects.findWidgetByType("digital-clock");
+if (datetime != null && typeof datetime != "undefined") {
+   datetime.writeConfig("defaultTimezone","Europe/Amsterdam");
+   datetime.writeConfig("timeZones", "Europe/Amsterdam,UTC");
+   datetime.writeConfig("timezone", "Europe/Amsterdam");
+   datetime.writeConfig("showTimezone", false);
+   print("DateTime widget configured, index: " + datetime.index);
+} else {
+   print("Unable to configure datetime widget");
+}
