6bf58ca1b29d7bb4eca8fbc6a4d8d7ce653e202d
Diederik de Haas <github@cknow.org>
2013-12-06 01:49:41 +0100

    Removed UTC from timeZones setting, apparently it's included either way.

--- apps/plasma-desktop/init/52-config-digitalclock.js
+++ apps/plasma-desktop/init/52-config-digitalclock.js
@@ -3,7 +3,7 @@ var findObjects = loadTemplate('org.cknow.plasma-desktop-scripting.findObjects')
 var datetime = findObjects.findWidgetByType("digital-clock");
 if (datetime != null && typeof datetime != "undefined") {
    datetime.writeConfig("defaultTimezone","Europe/Amsterdam");
-   datetime.writeConfig("timeZones", "Europe/Amsterdam,UTC");
+   datetime.writeConfig("timeZones", "Europe/Amsterdam");
    datetime.writeConfig("timezone", "Europe/Amsterdam");
    datetime.writeConfig("showTimezone", false);
    print("DateTime widget configured, index: " + datetime.index);
